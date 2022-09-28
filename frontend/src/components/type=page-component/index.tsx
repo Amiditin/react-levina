e3398ae7c3c906e68fa0963fdba271c08c8337ab
Dmitry Fomin <dimafomin02@gmail.com>
2022-09-28 21:21:42 +0300

    Fix

--- frontend/src/components/type=page-component/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './TypePageComponent.module.scss';
-
-interface TypePageComponentProps {}
-
-export const TypePageComponent: React.FC<TypePageComponentProps> = () => {
-  return <div className={styles.typePageComponent}>TypePageComponent Component</div>;
-};
