e3526e23059abc9b69441dc355a9fa7601b091c5
Dmitry Fomin <dimafomin02@gmail.com>
2022-09-30 00:54:12 +0300

    Fixed rerenders Collection

--- frontend/src/components/Collection/Skeleton.tsx
+++ frontend/src/components/Collection/Skeleton.tsx
@@ -20,33 +20,4 @@ const Skeleton: React.FC<SkeletonProps> = ({ size }) => {
   );
 };
 
-// interface SkeletonProps {
-//   // size: number;
-// }
-
-// const Skeleton: React.FC<SkeletonProps> = () => {
-//   return (
-//     // <ContentLoader
-//     //   style={{ minWidth: 350 }}
-//     //   width="350"
-//     //   height="350"
-//     //   viewBox="0 0 350 350"
-//     //   title="Загрузка..."
-//     //   backgroundColor="#f0f0f0"
-//     //   foregroundColor="#dedede">
-//     //   <rect x="380" y="0" rx="0" ry="0" width="350" height="350" />
-//     // </ContentLoader>
-//     <ContentLoader
-//       style={{ minWidth: 350 }}
-//       width={350}
-//       height={350}
-//       viewBox="0 0 350 350"
-//       title="Загрузка..."
-//       backgroundColor="#f0f0f0"
-//       foregroundColor="#dedede">
-//       <rect x="0" y="0" rx="0" ry="0" width="356" height="350" />
-//     </ContentLoader>
-//   );
-// };
-
 export default Skeleton;
--- frontend/src/components/Collection/index.tsx
+++ frontend/src/components/Collection/index.tsx
@@ -9,41 +9,48 @@ import Skeleton from './Skeleton';
 
 interface CollectionProps {}
 
+interface Arrow {
+  leftDisabled: boolean;
+  rightDisabled: boolean;
+}
+
 export const Collection: React.FC<CollectionProps> = () => {
   const numberItems = 10;
   const { items, status } = useAppSelector((state) => state.posts);
-  const [collectionScrollLeft, setCollectionScrollLeft] = React.useState(0);
+  const size = useWindowSize();
+  const [arrowStatus, setArrowStatus] = React.useState<Arrow>({
+    leftDisabled: true,
+    rightDisabled: false,
+  });
 
   const itemsRef = React.useRef<HTMLDivElement>(null);
-  const size = useWindowSize();
-
-  React.useEffect(() => {
-    if (itemsRef.current) setCollectionScrollLeft(itemsRef.current.scrollLeft);
-  }, [itemsRef]);
 
   const onClickArrow = (offset: number) => {
     if (itemsRef.current) {
-      let scrollLeft = collectionScrollLeft + offset;
+      let scrollLeft = itemsRef.current.scrollLeft + offset;
       let maxScrollWidth = itemsRef.current.scrollWidth - itemsRef.current.clientWidth;
 
       if (scrollLeft > 0 && scrollLeft < maxScrollWidth) {
-        setCollectionScrollLeft(scrollLeft);
         itemsRef.current.scroll({ left: scrollLeft, behavior: 'smooth' });
+
+        if (arrowStatus.leftDisabled || arrowStatus.rightDisabled) {
+          setArrowStatus({ leftDisabled: false, rightDisabled: false });
+        }
       } //
-      else if (scrollLeft >= maxScrollWidth && collectionScrollLeft !== maxScrollWidth) {
-        setCollectionScrollLeft(maxScrollWidth);
+      else if (scrollLeft >= maxScrollWidth && itemsRef.current.scrollLeft !== maxScrollWidth) {
         itemsRef.current.scroll({ left: maxScrollWidth, behavior: 'smooth' });
+        setArrowStatus({ ...arrowStatus, rightDisabled: true });
       } //
-      else if (scrollLeft <= 0 && collectionScrollLeft !== 0) {
-        setCollectionScrollLeft(0);
+      else if (scrollLeft <= 0 && itemsRef.current.scrollLeft !== 0) {
         itemsRef.current.scroll({ left: 0, behavior: 'smooth' });
+        setArrowStatus({ ...arrowStatus, leftDisabled: true });
       }
     }
   };
 
   const ArrowLeft = (): JSX.Element => (
     <div
-      className={clsx(styles.arrow, collectionScrollLeft === 0 && styles.disabled)}
+      className={clsx(styles.arrow, arrowStatus.leftDisabled && styles.disabled)}
       onClick={() => onClickArrow(size.width <= 560 ? -330 : -380)}>
       <img src="/img/tools-icons/arrow-big-left.svg" alt="arrow" />
     </div>
@@ -75,13 +82,7 @@ export const Collection: React.FC<CollectionProps> = () => {
         )}
         {size.width <= 560 && <ArrowLeft />}
         <div
-          className={clsx(
-            styles.arrow,
-            itemsRef.current &&
-              collectionScrollLeft ===
-                itemsRef.current.scrollWidth - itemsRef.current.clientWidth &&
-              styles.disabled,
-          )}
+          className={clsx(styles.arrow, arrowStatus.rightDisabled && styles.disabled)}
           onClick={() => onClickArrow(size.width <= 560 ? 330 : 380)}>
           <img src="/img/tools-icons/arrow-big-right.svg" alt="arrow" />
         </div>
@@ -89,5 +90,3 @@ export const Collection: React.FC<CollectionProps> = () => {
     </div>
   );
 };
-
-// TODO: переделать collectionScrollLeft на useRef
